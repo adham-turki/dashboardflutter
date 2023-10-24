b0d2f529f1f41e2b50c7ff9b628668703e6dcf71
Dania-Alqam <1180950@student.birzeit.edu>
2023-10-24 11:26:07 +0300

    language widget login screen

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -1,20 +1,18 @@
 import 'package:bi_replicate/controller/login/login_controller.dart';
-import 'package:bi_replicate/home.dart';
 import 'package:bi_replicate/model/login/users_model.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-
+import 'package:provider/provider.dart';
 import '../Encryption/encryption.dart';
 import '../components/login_components/custom_painter.dart';
 import '../components/login_components/form_component.dart';
 import '../controller/central_api_controller.dart';
 import '../controller/error_controller.dart';
+import '../provider/local_provider.dart';
 import '../utils/constants/constants.dart';
 import '../utils/constants/encrypt_key.dart';
+import '../widget/language_widget.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -36,6 +34,7 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     height = MediaQuery.of(context).size.height;
     width = MediaQuery.of(context).size.width;
+    final localeProvider = Provider.of<LocaleProvider>(context);
 
     return Scaffold(
         body: Stack(
@@ -78,6 +77,18 @@ class _LoginScreenState extends State<LoginScreen> {
                   Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Align(
+                          alignment: Alignment.center,
+                          child: LanguageWidget(
+                            color: Colors.black,
+                            onLocaleChanged: (locale) {
+                              localeProvider.setLocale(locale);
+                            },
+                          ),
+                        ),
+                      ),
                       FormComponent(
                         aliasName: aliasName,
                         userController: userController,
