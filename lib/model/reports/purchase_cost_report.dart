91154dc9ac02f160ff48741b566a4a27aa827364
Dania-Alqam <1180950@student.birzeit.edu>
2023-10-26 11:04:47 +0300

    pluto widths

--- lib/model/reports/purchase_cost_report.dart
+++ lib/model/reports/purchase_cost_report.dart
@@ -154,7 +154,7 @@ class PurchaseCostReportModel {
         field: fieldsName[i],
         type: PlutoColumnType.text(),
         width: isDesktop
-            ? fieldsName[i] == 'dash' || fieldsName[i] == "branch"
+            ? fieldsName[i] == 'dash'
                 ? width * .04
                 : width * .13
             : width * 0.3,
