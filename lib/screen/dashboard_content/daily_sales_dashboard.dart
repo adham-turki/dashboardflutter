f7508c92d6082910117cd9b5aa7578769bff2c9b
Dania-Alqam <1180950@student.birzeit.edu>
2023-11-05 15:19:46 +0200

    dashboard edits

--- lib/screen/dashboard_content/daily_sales_dashboard.dart
+++ lib/screen/dashboard_content/daily_sales_dashboard.dart
@@ -75,7 +75,7 @@ class _DailySalesDashboardState extends State<DailySalesDashboard> {
     'Save as JPEG',
     'Save as PNG',
   ];
-  //List<BarData> barDataTest = [];
+  List<BarData> barDataTest = [];
 
   List<PieChartModel> pieData = [];
   String accountNameString = "";
@@ -107,12 +107,12 @@ class _DailySalesDashboardState extends State<DailySalesDashboard> {
 
   @override
   void initState() {
-    // for (int i = 0; i < 100; i++) {
-    //   barDataTest.add(BarData(
-    //     name: 'Bar $i',
-    //     percent: Random().nextDouble() * 100,
-    //   ));
-    // }
+    for (int i = 0; i < 100; i++) {
+      barDataTest.add(BarData(
+        name: 'Bar $i',
+        percent: Random().nextDouble() * 100,
+      ));
+    }
     getPayableAccounts(isStart: true).then((value) {
       payableAccounts = value;
       setState(() {});
