92967cb13f5810a06d3eb8c956e1f4291c04adc2
Dania-Alqam <1180950@student.birzeit.edu>
2023-11-06 09:43:04 +0200

    dashboard mobile v

--- lib/screen/dashboard_content/filter_dialog/filter_dialog_sales_by_cat.dart
+++ lib/screen/dashboard_content/filter_dialog/filter_dialog_sales_by_cat.dart
@@ -346,7 +346,7 @@ class _FilterDialogSalesByCategoryState
           children: [
             Components().blueButton(
               height: width > 800 ? height * .05 : height * .06,
-              fontSize: width > 800 ? height * .016 : height * .011,
+              fontSize: width > 800 ? height * .016 : height * .015,
               width: isDesktop ? width * 0.09 : width * 0.25,
               onPressed: () {
                 DateTime from = DateTime.parse(_fromDateController.text);
