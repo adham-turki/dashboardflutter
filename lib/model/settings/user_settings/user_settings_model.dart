03a0155267888bf744dc8981546c9f06d1c17e0f
Mostafa Hameda <mostafaxd12@gmail.com>
2023-11-12 15:10:44 +0200

    Add Table To Users Screen

--- lib/model/settings/user_settings/user_settings_model.dart
+++ lib/model/settings/user_settings/user_settings_model.dart
@@ -1,35 +1,84 @@
-class UserSettingsModel {
-  String txtKey;
-  String txtReportcode;
-  String txtUsercode;
-  String txtFiltercode;
-  String txtFiltervalue;
+import 'package:flutter/cupertino.dart';
+import 'package:pluto_grid/pluto_grid.dart';
 
-  UserSettingsModel({
-    required this.txtKey,
-    required this.txtReportcode,
-    required this.txtUsercode,
-    required this.txtFiltercode,
-    required this.txtFiltervalue,
+import '../../../utils/constants/colors.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import '../../../utils/constants/responsive.dart';
+
+class UsersModel {
+  String code;
+  String username;
+  String password;
+  String activeToken;
+  String role;
+
+  UsersModel({
+    required this.code,
+    required this.username,
+    required this.password,
+    required this.activeToken,
+    required this.role,
   });
 
-  factory UserSettingsModel.fromJson(Map<String, dynamic> json) {
-    return UserSettingsModel(
-      txtKey: json['txtKey'],
-      txtReportcode: json['txtReportcode'],
-      txtUsercode: json['txtUsercode'],
-      txtFiltercode: json['txtFiltercode'],
-      txtFiltervalue: json['txtFiltervalue'],
+  factory UsersModel.fromJson(Map<String, dynamic> json) {
+    return UsersModel(
+      code: json['code'],
+      username: json['username'],
+      password: json['password'],
+      activeToken: json['activeToken'],
+      role: json['role'],
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
-      'txtKey': txtKey,
-      'txtReportcode': txtReportcode,
-      'txtUsercode': txtUsercode,
-      'txtFiltercode': txtFiltercode,
-      'txtFiltervalue': txtFiltervalue,
+      'code': code,
+      'username': username,
+      'password': password,
+      'activeToken': activeToken,
+      'role': role,
     };
+  }
+
+  static List<PlutoColumn> getColumns(
+      BuildContext context, AppLocalizations localizations) {
+    double width = MediaQuery.of(context).size.width;
+    bool isDesktop = Responsive.isDesktop(context);
+
+    List<PlutoColumn> list = [
+      // PlutoColumn(
+      //   title: "#",
+      //   field: "dash",
+      //   type: PlutoColumnType.number(),
+      //   width: isDesktop ? width * 0.04 : width * 0.1,
+      //   backgroundColor: colColor,
+      // ),
+      PlutoColumn(
+        title: localizations.userName,
+        field: "username",
+        type: PlutoColumnType.text(),
+        width: isDesktop ? width * 0.3 : width * 0.45,
+        backgroundColor: colColor,
+      ),
+      PlutoColumn(
+        title: localizations.role,
+        field: "role",
+        type: PlutoColumnType.text(),
+        width: isDesktop ? width * 0.3 : width * 0.45,
+        backgroundColor: colColor,
+      ),
+    ];
+    return list;
+  }
+
+  PlutoRow toPluto(int dash) {
+    final Map<String, PlutoCell> salesReport = <String, PlutoCell>{};
+
+    // salesReport['dash'] = PlutoCell(value: dash);
+    salesReport['username'] = PlutoCell(value: username ?? "");
+    salesReport['role'] = PlutoCell(value: role ?? "");
+    // salesReport['accountType'] = PlutoCell(value: accountType ?? "");
+
+    return PlutoRow(cells: salesReport);
   }
 }
