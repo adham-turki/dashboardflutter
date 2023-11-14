80d0c212e4dbf5d1495bca4b2063da94da03279a
Dania-Alqam <1180950@student.birzeit.edu>
2023-11-14 10:56:12 +0200

    dashboard

--- lib/components/charts.dart
+++ lib/components/charts.dart
@@ -45,7 +45,8 @@ class CustomBarChart extends StatelessWidget {
                 scrollDirection: Axis.horizontal,
                 child: Container(
                   height: isDesktop ? height * 0.423 : height * 0.5,
-                  width: data.length * 100,
+
+                  width: data.length * 130,
                   //    padding: const EdgeInsets.all(16.0),
                   child: SfCartesianChart(
                     isTransposed: true,
